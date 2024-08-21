5ebabe03d5cee15468ade923644000b863cff06c
John Wesely <john.wesely@tappnetwork.com>
2024-08-21 14:25:47 -0600

    add css selector for form user container

--- resources/views/livewire/filament-form-user/show.blade.php
+++ resources/views/livewire/filament-form-user/show.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-row justify-center pt-16 filament-form-builder">
-    <div class="border rounded-xl py-4 px-8">
+    <div class="filament-form-user-container border rounded-xl py-4 px-8">
         {{ $this->entryInfoList }}
     </div>
 </div>
