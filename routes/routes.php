f9e3820a1422e9551e69c257415eee4a79cef1b3
scott grayson <Scott Grayson>
2025-04-08 11:11:26 -0400

    remove "filament" from routes and make it configurable

--- routes/routes.php
+++ routes/routes.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Tapp\FilamentFormBuilder\Livewire\FilamentForm\Show as FilamentFormShow;
 use Tapp\FilamentFormBuilder\Livewire\FilamentFormUser\Show as FilamentFormUserShow;
 
-Route::get('filament-form-users/{entry}', FilamentFormUserShow::class)
+Route::get(config('filament-form-builder.filament-form-user-uri') . '/{entry}', FilamentFormUserShow::class)
     ->middleware('web')
     ->name('filament-form-users.show');
 
-Route::get('filament-form-builder/{form}', FilamentFormShow::class)
+Route::get(config('filament-form-builder.filament-form-uri') . '/{form}', FilamentFormShow::class)
     ->middleware('web')
     ->name('filament-form-builder.show');
