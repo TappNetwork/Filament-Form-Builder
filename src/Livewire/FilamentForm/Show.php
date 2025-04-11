2874dad268f205e50335c756790ecbf6595839c8
John Wesely <john.wesely@tappnetwork.com>
2025-04-11 11:24:20 -0600

    add preview feature

--- src/Livewire/FilamentForm/Show.php
+++ src/Livewire/FilamentForm/Show.php
@@ -23,10 +23,14 @@ class Show extends Component implements HasForms
 
     public bool $blockRedirect;
 
+    public bool $preview;
+
     public ?array $data = [];
 
-    public function mount(FilamentForm $form, bool $blockRedirect = false)
+    public function mount(FilamentForm $form, bool $blockRedirect = false, bool $preview = false)
     {
+        $this->preview = $preview;
+
         $this->filamentForm = $form->load('filamentFormFields');
 
         $this->form->fill($this->data);
@@ -102,6 +106,10 @@ class Show extends Component implements HasForms
     public function create()
     {
         $this->form->getState();
+
+        if ($this->preview) {
+            return;
+        }
 
         $entry = [];
 
