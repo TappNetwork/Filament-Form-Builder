c2c73ef45f4ff7a4bf5064092e0a6a0312bae0c2
Steve Williamson <steve@sjdub.com>
2025-07-03 15:56:40 -0400

    use the filament button component

--- resources/views/livewire/filament-form/show.blade.php
+++ resources/views/livewire/filament-form/show.blade.php
@@ -1,6 +1,6 @@
-<div class="p-16 flex flex-row justify-center fb-form-component filament-form-builder">
+<div class="flex flex-row justify-center p-16 fb-form-component filament-form-builder">
     <div class="max-w-[600px] min-w-[400px] rounded-xl border-2 p-4 fb-form-container">
-        <h1 class="font-bold text-xl mb-2">
+        <h1 class="mb-2 text-xl font-bold">
             {{ $this->filamentForm->name }}
         </h1>
         <p class="mb-4">
@@ -10,12 +10,9 @@
             @csrf
             {{ $this->form }}
 
-            <button
-                type="submit"
-                class="filament-form-builder fb-form-submit-button bg-emerald-400 hover:bg-emerald-600 hover:scale-[1.03] shadow-lg px-2 py-1 mt-4 rounded-md text-white"
-            >
+            <x-filament::button type="submit">
                 Submit
-            </button>
+            </x-filament::button>
         </form>
 
         <x-filament-actions::modals />
