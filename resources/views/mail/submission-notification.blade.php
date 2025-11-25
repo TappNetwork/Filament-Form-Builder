a28ea0738cad9bc1d525498aef248370cc66455c
scott grayson <Scott Grayson>
2025-11-25 15:28:54 -0500

    Update notification email to link to form edit page instead of single entry
    - Change button URL to form resource edit page
    - Update button text to 'View Form & Entries'
    - Admins prefer to export entries from the form page

--- resources/views/mail/submission-notification.blade.php
+++ resources/views/mail/submission-notification.blade.php
@@ -6,8 +6,8 @@ A new submission has been received for **{{ $form->name }}**.
 **Submitted by:** {{ $entry->user?->name ?? 'Guest' }}  
 **Submitted at:** {{ $entry->created_at->format('F j, Y g:i A') }}
 
-<x-mail::button :url="route(config('filament-form-builder.filament-form-user-show-route'), $entry)">
-View Submission
+<x-mail::button :url="route('filament.admin.resources.filament-forms.edit', $form)">
+View Form & Entries
 </x-mail::button>
 
 Thanks,<br>
