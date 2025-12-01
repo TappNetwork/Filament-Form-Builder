d1c597575412f4de8cdc1ff57ae35b96d0f72277
scott grayson <Scott Grayson>
2025-12-01 16:49:01 -0500

    Remove submission details from notification email
    Keep email simple with just form name, submitter, and timestamp

--- resources/views/mail/submission-notification.blade.php
+++ resources/views/mail/submission-notification.blade.php
@@ -7,23 +7,6 @@ A new submission has been received for **{{ $form->name }}**.
 
 **Submitted at:** {{ $entry->created_at->format('F j, Y g:i A') }}
 
----
-
-## Submission Details
-
-@foreach($entry->entry as $field)
-**{{ $field['field'] }}**
-
-@if(is_array($field['answer']))
-{{ implode(', ', $field['answer']) }}
-@else
-{{ $field['answer'] ?: '(No answer provided)' }}
-@endif
-
-@endforeach
-
----
-
 <x-mail::button :url="route('filament.admin.resources.filament-forms.edit', $form)">
 View Form & Entries
 </x-mail::button>
